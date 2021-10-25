254fea290a5889c2cacc9e255149fc12fda39942
18jad <jadoyahya@gmail.com>
2021-10-25 17:02:35 +0300

    Palindrome Checeker

---
+++ js/checker.js
@@ -0,0 +1,43 @@
+var input = document.getElementById("checker"), form = document.getElementById("checker-form"), btn = document.getElementById("checker-btn"), word = document.getElementById("pal-word"), ans = document.getElementById("pal-ans"), green = document.getElementById("bg-green"), red = document.getElementById("bg-red");
+var isPalindrome = function (str) {
+    str = str.replace(/[\W+|_]/g, "").toLowerCase();
+    return str === str.split("").reverse().join("");
+};
+form.addEventListener("submit", function (e) {
+    e.preventDefault();
+    // @ts-ignore
+    if (input.value.replace(/[\W+|_]/g, "").toLowerCase() == "") {
+        word.textContent = "Please enter something";
+        ans.textContent = "";
+        input.style.boxShadow = "0px 2px 13px -2px rgba(255, 1, 1, 0.6)";
+        btn.style.boxShadow = "0px 2px 13px -2px rgba(255, 1, 1, 0.6)";
+        return;
+    }
+    else {
+        // @ts-ignore
+        word.textContent = input.value + " ";
+        // @ts-ignore
+        if (isPalindrome(input.value)) {
+            ans.textContent = "‎ ‎is palindrome!";
+            // body.style.backgroundImage = linearGreen;
+            if (red.classList.contains("opac")) {
+                red.classList.remove("opac");
+            }
+            green.classList.add("opac");
+            input.style.boxShadow = "0px 2px 13px -2px rgba(56, 245, 81, 1)";
+            btn.style.boxShadow = "0px 2px 13px -2px rgba(56, 245, 81, 1)";
+        }
+        else {
+            ans.textContent = "‎ ‎is not palindrome!";
+            if (green.classList.contains("opac")) {
+                green.classList.remove("opac");
+            }
+            red.classList.add("opac");
+            input.style.boxShadow = "0px 2px 13px -2px rgba(255, 1, 1, 0.6)";
+            btn.style.boxShadow = "0px 2px 13px -2px rgba(255, 1, 1, 0.6)";
+        }
+    }
+});
+setTimeout(function () {
+    document.body.classList.remove("preload");
+}, 500);
